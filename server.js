9e365c4167d4ed7c254f3cf61b4f3fb9005a4827
Parfait <jenny.filcheck@gmail.com>
2017-06-11 20:02:58 -0500

    configured cors client origin

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const cors = require('cors');
 // Environment Config /////////////////////////////////////////
 require('dotenv').config({ path: './config.dev.env' });
 
+// CORS Config ////////////////////////////////////////////////
+const corsOptions = { origin: process.env.CLIENT_ORIGIN, optionsSuccessStatus: 200 };
+
 // Mongoose Config ////////////////////////////////////////////
 mongoose.connect(process.env.DATABASE);
 mongoose.Promise = global.Promise;
@@ -18,7 +21,7 @@ mongoose.connection.on('error', (err) => {
 // App Config /////////////////////////////////////////////////
 const app = express();
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors(corsOptions));
 // additional logging in dev environment
 if (process.env.ENV === 'dev') { 
   app.use(morgan("combined")); 
